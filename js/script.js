0ab717a6f8b41ba17c4729656001ffefdfe89b98
Abdulazeez_A <97477565+shy-programmer@users.noreply.github.com>
2022-07-13 21:38:37 +0100

    designing button

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
           1 : {1 : "I", 2 : "II", 3 : "III", 4 : "IV", 5 : "V", 6 : "VI", 7 : "VII", 8 : "VIII", 9 : "IX"},
           2 : {1 : "X", 2 : "XX", 3 : "XXX", 4 : "XL", 5 : "L", 6 : "LX", 7 : "LXX", 8 : "LXXX", 9 : "XC"},
           3 : {1 : "C", 2 : "CC", 3 : "CCC", 4 : "CD", 5 : "D", 6 : "DC", 7 : "DCC", 8 : "DCCC", 9 : "CM"},
-          4 : {1 : "M", 2 : "MM", 3 : "MMM"},
+          4 : {1 : "M", 2 : "MM", 3 : "MMM", 4 : "MMMM"},
         }
         
         let me = num.toString()
@@ -19,7 +19,13 @@
       
     function converter() {
       var ques = document.getElementById("gon").value;
-      var final = ques + " = " + convertToRoman(ques);
+
+    if (parseInt(ques) < 5000) {
+        var final = ques + " = " + convertToRoman(ques);
+    }
+    else if (parseInt(ques) >= 5000){
+        var final = "I'm sorry, the current limit is 4999... "
+    }
     document
       .getElementById("answer")
       .textContent = final; }
